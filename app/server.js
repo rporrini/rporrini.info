46d4c82b3991fe798a64082a18e58e408a9baf59
rporrini <r.porrini@gmail.com>
2014-03-23 19:45:04 +0100

    added support for static resources

--- app/server.js
+++ app/server.js
@@ -4,4 +4,5 @@ var app = express();
 app.get('/alive', function(req, res){
   res.send('OK');
 });
+app.use('/static', express.static(__dirname + '/assets'));
 exports.app = app;
--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,6 @@
 var request = require('supertest');
 var server = require('../app/server');
+var file = require('fs');
 
 describe('Application', function(){
 	describe('/alive', function(){
@@ -7,6 +8,19 @@ describe('Application', function(){
 			request(server.app)
 				.get('/alive')
 				.expect('OK', done);
+		});
+	});
+	describe('/static', function(){
+		before(function(){
+			file.writeFile('app/assets/file.txt', 'content');
+		});
+		after(function(){
+			file.unlink('app/assets/file.txt');
+		});
+		it('should mirror static resources', function(done){
+			request(server.app)
+				.get('/static/file.txt')
+				.expect(200, done);
 		});
 	});
 });
