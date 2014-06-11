4d8ef1b69612b145ad2e580d1aa519ff715e486d
rporrini <r.porrini@gmail.com>
2014-06-11 20:32:38 +0200

    now we use the jade template engine

--- app/server.js
+++ app/server.js
@@ -2,11 +2,14 @@ var express = require('express');
 var file = require('./filesystem');
 
 var app = express();
+app.set('views', file.pathOf('views'));
+app.set('view engine', 'jade');
+
 app.use('/static', express.static(file.pathOf('assets')))
    .get('/alive', function(req, res){
 	   	res.send('OK');
    })
    .get('/', function(req, res){
-	   	res.sendfile(file.pathOf('views/index.html'));
+	   res.render('index', {});
    });
 exports.app = app;
