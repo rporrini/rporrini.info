f038b625c3788d985b8055783b1b3613ca8a8354
rporrini <r.porrini@gmail.com>
2014-10-01 09:12:18 -0500

    added the installation of firefox to the bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,6 +9,10 @@ if ! command -v heroku ; then
 	echo 'Installing Heroku-Toolbelt'
 	curl https://toolbelt.heroku.com/install-ubuntu.sh | sh
 fi
+if ! command -v firefox ; then
+	echo 'Installing Firefox'
+	sudo apt-get install firefox
+fi
 
 relative_path=`dirname $0`
 directory=`cd $relative_path;pwd`
