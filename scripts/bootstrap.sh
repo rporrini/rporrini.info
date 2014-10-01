67cf0cf3d89e5523df6cdb2431d21045c88c9ee3
rporrini <r.porrini@gmail.com>
2014-10-01 09:37:59 -0500

    now the production environment is configured in the bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+relative_path=`dirname $0`
+directory=`cd $relative_path;pwd`
+
 if ! command -v node ; then
 	echo 'Installing Node'
 	sudo apt-get install nodejs nodejs-legacy npm
@@ -8,14 +11,15 @@ fi
 if ! command -v heroku ; then
 	echo 'Installing Heroku-Toolbelt'
 	curl https://toolbelt.heroku.com/install-ubuntu.sh | sh
+	cd $directory/..
+	heroku git:remote -a rporrini
+	heroku keys:add
 fi
 if ! command -v firefox ; then
 	echo 'Installing Firefox'
 	sudo apt-get install firefox
 fi
 
-relative_path=`dirname $0`
-directory=`cd $relative_path;pwd`
 cd $directory/..
 
 npm install
