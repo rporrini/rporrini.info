fce5a3545a6e38d39b10c57c622b705dacdcf142
rporrini <r.porrini@gmail.com>
2017-01-15 12:53:40 +0100

    cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,17 +5,12 @@ relative_path=`dirname $0`
 directory=`cd $relative_path;pwd`
 cd $directory/..
 
-npm test
-
-echo "****** pushing to $1 environment ******"
+echo "****** deploying to $1 environment ******"
 
 case $1 in
-	development)
-		git push origin
-		;;
 	staging)
 		git push staging master
-		./scripts/run-backend-tests.sh production rporrini-staging.herokuapp.com
+		./scripts/run-backend-tests.sh production stage.rporrini.info
 		;;
 	production)
 		git push heroku master
