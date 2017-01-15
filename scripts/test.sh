656404b69c31d3a8fc24220a9a4f10bac59df04a
rporrini <r.porrini@gmail.com>
2017-01-15 14:34:33 +0100

    cleanup

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-relative_path=`dirname $0`
-directory=`cd $relative_path;pwd`
-cd $directory
-cd ..
-
-export ENVIRONMENT=$2
-./node_modules/.bin/_mocha --timeout 60000 test/$1
--- web.js
+++
@@ -1,7 +0,0 @@
-var server = require('./app/server');
-
-var port = process.env.PORT || 5000;
-server.app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
