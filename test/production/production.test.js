0d49202b2f787be233641983f2108002d593ace1
rporrini <r.porrini@gmail.com>
2017-01-15 12:48:03 +0100

    dns for staging app

--- test/production/production.test.js
+++ test/production/production.test.js
@@ -1,34 +1,34 @@
 var request = require('superagent');
 var should = require('should');
 
-describe('application', function(){
-	describe('/', function(){
-		it('should be accessible', function(done){
+describe('application', function () {
+	describe('/', function () {
+		it('should be accessible', function (done) {
 			isAccessible('/', done);
 		});
 	});
-	describe('/alive', function(){
-		it('should be accessible', function(done){
+	describe('/alive', function () {
+		it('should be accessible', function (done) {
 			isAccessible('/alive', done);
 		});
 	});
-	describe('first post', function(){
-		it('should be accessible', function(done){
+	describe('first post', function () {
+		it('should be accessible', function (done) {
 			isAccessible('/blog/post/hello-world', done);
 		});
 	});
-	describe('/blog', function(){
-		it('should be accessible', function(done){
+	describe('/blog', function () {
+		it('should be accessible', function (done) {
 			isAccessible('/blog', done);
 		});
 	});
 });
 
-function isAccessible(path, done){
+function isAccessible(path, done) {
 	request
 		.get('http://' + process.env.ENVIRONMENT + path)
-		.end(function(res){
+		.end(function (res) {
 			res.status.should.equal(200);
 			done();
 		});
-};
+}
