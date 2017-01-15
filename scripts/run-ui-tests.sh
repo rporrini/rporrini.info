6e6d0ff6037be1ef544658ead0e314b70990882a
rporrini <r.porrini@gmail.com>
2017-01-15 13:04:37 +0100

    cleanup

--- scripts/run-ui-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-relative_path=`dirname $0`
-directory=`cd $relative_path;pwd`
-cd $directory
-cd ..
-
-./node_modules/karma/bin/karma start ./test/ui/karma.conf.js
--- test/ui/trivial.js
+++
@@ -1,4 +0,0 @@
-describe("a trivial test", function(){
-	it('should always pass', function(){
-	});
-});
