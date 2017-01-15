6e89e576b2380fc910419ebb509827f3c10a00fc
rporrini <r.porrini@gmail.com>
2017-01-15 16:37:27 +0100

    fixed the spaces in the homepage

--- app/blog.js
+++ app/blog.js
@@ -1,18 +1,18 @@
-var moment = require('moment')
+var moment = require('moment');
 
-exports.post = post = function(post){
+exports.post = post = function (post) {
 	return {
-		date : moment(post.date).format("MMMM Do, YYYY"),
-		title : post.title,
-		content : post.content,
-		url : post.url
+		date: moment(post.date).format("MMMM Do, YYYY"),
+		title: post.title,
+		content: post.content,
+		url: post.url
 	};
 };
 
-exports.posts = function(posts){
+exports.posts = function (posts) {
 	var result = [];
-	posts.forEach(function(p){
+	posts.forEach(function (p) {
 		result.push(post(p));
 	});
 	return result;
-}
+};
