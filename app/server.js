7e5c00298a47e02c0bbabd32866fb3a40fac45a6
rporrini <r.porrini@gmail.com>
2017-01-15 11:39:23 +0100

    deletions

--- app/server.js
+++ app/server.js
@@ -4,59 +4,49 @@ var fs = require('fs');
 var Poet = require('poet');
 var blog = require('./blog');
 
-var send404 = function(res){
+var send404 = function (res) {
 	res.send(404, 'There is nothing here!');
 };
 
 var app = express();
 
-var poet = Poet(app, {posts: file.pathOf('posts')});
-var hidden_poet = Poet(app, {posts: file.pathOf('hidden-posts')});
-
-hidden_poet.watch().init();
+var poet = Poet(app, {
+	posts: file.pathOf('posts')
+});
 poet.watch().init();
 
 app.set('views', file.pathOf('views'));
 app.set('view engine', 'jade');
 
 poet.addRoute('/blog/post/:post', function (req, res) {
-	  var post = poet.helpers.getPost(req.params.post);
-	  if (post) {
-	    res.render('post', { post: blog.post(post) }); 
-	  } else {
-		  send404(res);
-	  }
-});
-
-hidden_poet.addRoute('/hidden-blog/post/:post', function (req, res) {
-	  var post = hidden_poet.helpers.getPost(req.params.post);
-	  if (post) {
-	    res.render('hidden-post', { post: blog.post(post) }); 
-	  } else {
-		  send404(res);
-	  }
+	var post = poet.helpers.getPost(req.params.post);
+	if (post) {
+		res.render('post', {
+			post: blog.post(post)
+		});
+	} else {
+		send404(res);
+	}
 });
 
 app.use('/static', express.static(file.pathOf('assets')))
-	.get('/alive', function(req, res){
-	   	res.send('OK');
+	.get('/alive', function (req, res) {
+		res.send('OK');
 	})
-	.get('/', function(req, res){
+	.get('/', function (req, res) {
 		res.render('home', {});
 	})
-	.get('/blog', function(req, res){
-		var posts = poet.helpers.getPosts(0, 100)
-		res.render('blog', { posts: blog.posts(posts) });
+	.get('/blog', function (req, res) {
+		var posts = poet.helpers.getPosts(0, 100);
+		res.render('blog', {
+			posts: blog.posts(posts)
+		});
 	})
-	.get('/hidden-blog', function(req, res){
-		var posts = hidden_poet.helpers.getPosts(0, 100)
-		res.render('blog', { posts: blog.posts(posts) });
-	})
-	.get('*', function(req, res){
+	.get('*', function (req, res) {
 		send404(res);
 	})
-	.use(function(err, req, res, next){
-		console.log(err)
+	.use(function (err, req, res, next) {
+		console.log(err);
 		send404(res);
 	});
 
--- test/unit/server.test.js
+++ test/unit/server.test.js
@@ -5,93 +5,66 @@ var fs = require('fs');
 var should = require('should');
 var testdoubles = require('./testdoubles');
 
-describe('route', function(){
-	describe('/static', function(){
-		before(function(){
+describe('route', function () {
+	describe('/static', function () {
+		before(function () {
 			file.write('assets/file.txt', 'content', fs);
 		});
-		after(function(){
+		after(function () {
 			file.remove('assets/file.txt', fs);
 		});
-		it('should mirror static resources', function(done){
+		it('should mirror static resources', function (done) {
 			request(server.app)
 				.get('/static/file.txt')
 				.expect(200, done);
 		});
 	});
-	describe('/alive', function(){
-		it('should be accessible', function(done){
+	describe('/alive', function () {
+		it('should be accessible', function (done) {
 			request(server.app)
 				.get('/alive')
 				.expect('OK', done);
 		});
 	});
-	describe('a not existing page', function(){
-		it('should be not accessible', function(done){
+	describe('a not existing page', function () {
+		it('should be not accessible', function (done) {
 			request(server.app)
 				.get('/not-existing-page')
 				.expect(404, done);
 		});
 	});
-	describe('/', function(){
-		it('should be accessible', function(done){
+	describe('/', function () {
+		it('should be accessible', function (done) {
 			request(server.app)
 				.get('/')
 				.expect(200, done);
 		});
 	});
-	describe('blog', function(){
-		it('should be accessible', function(done){
+	describe('blog', function () {
+		it('should be accessible', function (done) {
 			request(server.app)
 				.get('/blog')
 				.expect(200, done);
 		});
 	});
-	describe('hidden-blog', function(){
-		it('should be accessible', function(done){
-			request(server.app)
-				.get('/hidden-blog')
-				.expect(200, done);
-		});
-	});
-	describe('existing posts', function(){
-		before(function(){
+	describe('existing posts', function () {
+		before(function () {
 			file.write('posts/post.md', '{{{"title" : "The Beautiful Post", "date" : "7-30-2012"}}}', fs);
 		});
-		after(function(){
+		after(function () {
 			file.remove('posts/post.md', fs);
 		});
-		it('should be accessible', function(done){
-			setTimeout(function() {
+		it('should be accessible', function (done) {
+			setTimeout(function () {
 				request(server.app)
 					.get('/blog/post/the-beautiful-post')
 					.expect(200, done);
 			}, 5);
 		});
-		it('should give 404 on not existing posts', function(done){
+		it('should give 404 on not existing posts', function (done) {
 			request(server.app)
-					.get('/blog/post/not-existing-post')
-					.expect(404, done);
-		});
-	});
-	describe('hidden posts', function(){
-		before(function(){
-			file.write('hidden-posts/post.jade', '{{{"title" : "The Hidden Post", "date" : "7-30-2012"}}}', fs);
-		});
-		after(function(){
-			file.remove('hidden-posts/post.jade', fs);
-		});
-		it('should be accessible', function(done){
-			setTimeout(function() {
-				request(server.app)
-					.get('/hidden-blog/post/the-hidden-post')
-					.expect(200, done);
-			}, 5);	
-		});
-		it('should give 404 on not existing posts', function(done){
-			request(server.app)
-					.get('/hidden-blog/post/not-existing-post')
-					.expect(404, done);
+				.get('/blog/post/not-existing-post')
+				.expect(404, done);
 		});
 	});
 });
